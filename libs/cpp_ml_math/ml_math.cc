45c354b7bfdde8ef5fb8a627db953f3adaf46e3d
Jose Morgan <josegm11194@gmail.com>
2017-01-11 00:35:51 -0500

    initial commit

---
+++ libs/cpp_ml_math/ml_math.cc
@@ -0,0 +1,30 @@
+#include <node.h>
+
+namespace ml_math {
+
+using v8::FunctionCallbackInfo;
+using v8::Value;
+
+void Transpose(const FunctionCallbackInfo<Value>& args) {
+
+}
+
+void Determinant(const FunctionCallbackInfo<Value>& args) {
+
+}
+
+void Inverse(const FunctionCallbackInfo<Value>& args) {
+
+}
+
+void Dot(const FunctionCallbackInfo<Value>& args) {
+
+}
+
+} 
+
+
+
+
+
+
